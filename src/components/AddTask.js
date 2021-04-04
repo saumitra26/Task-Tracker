069f9c80b29daffab415274f91f39e4bc50e6a5b
Saumitra Bagchi <saumitra.bagchi@ericsson.com>
2021-04-04 09:22:35 +0300

    add task form creation with style

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const AddTask = () => {
     return (
-        <div>
-            ddd
-        </div>
+        <form>
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task'/>
+            </div>
+            <div className='form-control'>
+                <label>Day & Time</label>
+                <input type='text' placeholder=' Add day & Time'/>
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' />
+            </div>
+            <input type='submit' value='Save Task' className='btn btn-block'/>
+        </form>
     )
 }
 
